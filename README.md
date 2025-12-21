9f14416176ba414e6158a11a6409b03bba2c725b
martinranzuglia <martinranzuglia@gmail.com>
2025-12-21 11:25:02 -0300

    Realización del ejercicio 3.10

---
+++ README.md
@@ -0,0 +1,3 @@
+# Back End 
+
+### 🌐[Deploy link](https://fullstackopen-part3-backend-20ta.onrender.com/api/persons)
--- index.js
+++ index.js
@@ -108,6 +108,6 @@ app.post("/api/persons", (req, res) => {
 
 })
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Servidor corriendo en el puerto: ${PORT}`));
